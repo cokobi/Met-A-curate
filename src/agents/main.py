5b3838e7bf0099a0aaa31252fb96b19333f8546b
itamarb <itamarb@mobileye.com>
2026-03-18 18:14:32 +0200

    add MongoDB

--- src/agents/main.py
+++ src/agents/main.py
@@ -1,4 +1,3 @@
-# maim.pyimport os
 import os
 import time
 import uuid
@@ -19,6 +18,27 @@ class ToyAgent:
         if not tavily_key:
             raise ValueError("Missing TAVILY_API_KEY in .env")
         self.tavily_client = TavilyClient(api_key=tavily_key)
+
+        # Mongo Setup
+        mongo_uri = os.getenv("MONGO_URI")
+        if not mongo_uri:
+            raise ValueError("Missing MONGO_URI in .env")
+
+        try:
+            self.mongo_client = MongoClient(mongo_uri, tlsCAFile=certifi.where())
+            self.db = self.mongo_client["tavily_assignment"]
+            self.collection = self.db["agent_metadata"]
+            print("Connected to MongoDB successfully")
+        except Exception as e:
+            print(f"Failed to connect to MongoDB: {e}")
+
+        # # AWS Firehose Setup
+        # try:
+        #     self.firehose = boto3.client("firehose", aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"), aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"), region_name=os.getenv("AWS_REGION"))
+        #     self.stream_name = os.getenv("FIREHOSE_STREAM_NAME")
+        #     print(f"✅ Connected to AWS Firehose: {self.stream_name}")
+        # except Exception as e:
+        #     print(f"Failed to connect to AWS: {e}")
 
     def run_task(self, query):
         print(f"🔎 Agent is searching for: '{query}'...")
@@ -41,7 +61,28 @@ class ToyAgent:
             "full_response": response,
         }
 
+        self.save_to_mongo(metadata)
+        # self.send_to_firehose(metadata)
+
         return metadata
+
+    def save_to_mongo(self, data):
+        try:
+            if hasattr(self, "collection"):
+                result = self.collection.insert_one(data.copy())
+                print(f"💾 Saved to MongoDB with ID: {result.inserted_id}")
+        except Exception as e:
+            print(f"⚠️ Error saving to Mongo: {e}")
+
+    # def send_to_firehose(self, data):
+    #     try:
+    #         # Convert to JSON and add a newline (critical for Snowflake!)
+    #         payload = json.dumps(data) + "\n"
+
+    #         response = self.firehose.put_record(DeliveryStreamName=self.stream_name, Record={"Data": payload})
+    #         print(f"🚀 Sent to Firehose! Record ID: {response['RecordId']}")
+    #     except Exception as e:
+    #         print(f"⚠️ Error sending to Firehose: {e}")
 
 
 if __name__ == "__main__":
